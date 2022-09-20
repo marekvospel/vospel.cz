f531e8c9d2d45f639a21be7b9bc16a131d564f23
Marek Vospěl <marek@vospel.cz>
2022-09-20 18:52:30 +0200

    feat: add about me section

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -12,7 +12,7 @@
   <div class="flex flex-row items-center gap-2 ml-auto">
     <a href="https://blog.vospel.cz" class="text-gray-500 hover:text-cyan-400 focus-visible:text-cyan-400">Blog</a>
     <br />
-    <a href="https://github.com/marekvospel/blog" target="_blank" rel="noopener noreferrer" class="text-2xl group">
+    <a href="https://github.com/marekvospel" target="_blank" rel="noopener noreferrer" class="text-2xl group">
       <MdiGithub class="text-gray-500 hover:text-cyan-400 group-focus:text-cyan-400 transition-color duration-300"/>
     </a>
     <a href="https://twitter.com/marekvospel" target="_blank" rel="noopener noreferrer" class="text-2xl group">
